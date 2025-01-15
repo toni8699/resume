63f2ee6a6ec2163da71214b3d5b42de68ab02039
Tony <34341990+toni8699@users.noreply.github.com>
2025-01-14 22:49:03 -0500

    nav bar

---
+++ src/Sections/NavBar.jsx
@@ -0,0 +1,56 @@
+import 'react'
+import {useState} from "react";
+import {navLinks} from "../constants/index.js";
+
+const NavItems = () => {
+    return (
+        <ul className={"nav-ul"}>
+            {navLinks.map((link) => (
+                <li key={link.id} className={"nav-li"}>
+                    <a href={link.href} className={"nav-li_a"}
+                       onClick={ () =>{}}>
+                        {link.name}
+                    </a>
+                </li>
+            ))}
+        </ul>
+    )
+}
+const NavBar = () => {
+        const [isOpen,setIsOpen] = useState(false);
+
+    function toggleMenu() {
+        setIsOpen(prevState => !prevState);
+    }
+
+    return (
+        <header id={"navbar"} className={"fixed top-0 left-0 right-0 z-50" +
+            "bg-black/30 text-white"}>
+            <div className={"max-w-7xl mx-auto "}>
+                <div className={"flex justify-between items-center" +
+                    " py-5 mx-auto c-space"}>
+                    <a href={"./"} className={"text-neutral-400 font-bold text-xl " +
+                        "hover:text-white font-handwriting"}>
+                       <h3 className={'gradient-text hover:text-white'}>
+                           Khang Le</h3>
+                    </a>
+                    <button onClick ={toggleMenu} className={"sm:hidden flex text-neutral-400 hover:text-white focus:outline-none"} >
+                        <img src ={ isOpen? "assets/cancel.svg":"assets/menu-1.svg"} alt="menu icon"
+                        className={"w-6 h-6 " }/>
+                    </button>
+                    <nav className ="sm:flex hidden">
+                        <NavItems />
+                    </nav>
+                </div>
+
+
+            </div>
+            <div className={`nav-sidebar ${isOpen ? "translate-x-0" : "translate-x-full"}`}>
+                    <nav>
+                        <NavItems />
+                    </nav>
+            </div>
+        </header>
+    )
+}
+export default NavBar
